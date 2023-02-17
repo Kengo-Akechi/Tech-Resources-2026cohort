fdd29520c33b879eab4f182b64e3f3719949a742
Kengo-Akechi <122290376+Kengo-Akechi@users.noreply.github.com>
2023-02-17 23:57:12 +0530

    Submitting Q6 of Assignment 1

--- a1_q6.cpp
+++ a1_q6.cpp
@@ -4,17 +4,13 @@ using namespace std;
 
 int main (){
     int Num;
-    string X= to_string(Num);
+    cout<<"Enter the number"<<endl;
+    cin>>Num;
+    string X;
+    X= to_string(Num);
 
-    if (1<X.length()<10^9)
-    {
-        
-        cout<<"Enter the number"<<endl;
-       
-        cin>>Num;
-
-        cout<<X.length();
+    if ( 1<=X.length()< 10^9){
+    cout<<X.length(); 
     }
-
     return 0;
 }
