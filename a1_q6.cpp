397c30ff4f3e3d89299276f704c7d426fad561b5
Kengo-Akechi <122290376+Kengo-Akechi@users.noreply.github.com>
2023-02-17 23:25:07 +0530

    Update a1_q6.cpp

--- a1_q6.cpp
+++ a1_q6.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main (){
-    string Num;
-    cout<<"Enter the number"<<endl;
-    cin>>Num;
-    cout<<Num.length();
+    int Num;
+    string X= to_string(Num);
+
+    if (1<X.length()<10^9)
+    {
+        
+        cout<<"Enter the number"<<endl;
+       
+        cin>>Num;
+
+        cout<<X.length();
+    }
 
     return 0;
 }
